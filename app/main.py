0cf70db112a7ea7e34177c2d676f30ebcc2946c0
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-04-12 17:14:21 +0900

    test

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ name = "minjun"
 def read_root():
     return {"Hello": "World1"}
 
-#@app.get("/items/{item_id}")
-#def read_item(item_id: int, q: Union[str, None] = None):
-#    return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 @app.post("/")
 def create_root():
