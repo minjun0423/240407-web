f92b99a8a96c9021291ec702ba4c3776bd3a2f51
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-04-12 16:56:14 +0900

    test

--- app/main.py
+++ app/main.py
@@ -4,12 +4,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+name = "minjun"
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/")
+def create_root():
+    return{"hello":"post"}
+
+@app.put
+def update_root():
+    return{"hello":"put"}
+
+@app.delete("/")
+def delete_root():
+    return {"hello":"delete"}
