12cfc169fe262b4eca2ee379a050d0297b6a89bb
minjun0423 <162403690+minjun0423@users.noreply.github.com>
2024-04-12 17:21:55 +0900

    test2

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-name = "minjun"
-
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+    
 @app.get("/")
 def read_root():
-    return {"Hello": "World1"}
+    return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.post("/")
-def create_root():
-    return{"hello":"post"}
-
-@app.put("/")
-def update_root():
-    return{"hello":"put"}
-
-@app.delete("/")
-def delete_root():
-    return {"hello":"delete"}
+@app.post("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return{"item_name": item.name, "item_id": item_id}
